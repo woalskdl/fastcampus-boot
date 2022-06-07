76cee39d31b357b6d1103fbf8af3d14a98da71b9
jay <jay.wekeep@gmail.com>
2022-06-07 09:25:19 +0900

    아직 시작

---
+++ src/test/java/com/fastcampus/javaoop/MainTest.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Main.main(null);
+    }
+}
