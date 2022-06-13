a2ef94fd2e32488872e6d3d7d8be75dbe1a4e105
jay <jay.wekeep@gmail.com>
2022-06-14 08:39:52 +0900

    실행될 메인 로직 작성 및 정렬 알고리즘 사용

--- src/test/java/com/fastcampus/javaoop/MainTest.java
+++ src/test/java/com/fastcampus/javaoop/MainTest.java
@@ -8,6 +8,10 @@ class MainTest {
 
     @Test
     void main() {
-        Main.main(null);
+        // Given
+        String[] args = {"3", "1", "2"};
+
+        // When & Then
+        Main.main(args);
     }
 }
