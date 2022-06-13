973aa90d584847c3b242fdb978ef445c69691f31
jay <jay.wekeep@gmail.com>
2022-06-14 08:38:45 +0900

    버블소트와 테스트 구현

--- src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/BubbleSort.java
@@ -9,7 +9,7 @@ public class BubbleSort <T extends Comparable<T>> {
         List<T> output = new ArrayList<>(list);
 
         for(int i = output.size() - 1; i > 0; i-- ) {
-            for(int j = 0; j < i; i++) {
+            for(int j = 0; j < i; j++) {
                 if(output.get(j).compareTo(output.get(j + 1)) > 0) {
                     T temp = output.get(j);
                     output.set(j, output.get(j + 1));
